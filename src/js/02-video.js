a58789028f8383aa61d4d9686dda4261690bfa46
Denys Lavryk <121813691+strictly-rigid@users.noreply.github.com>
2023-05-14 20:59:45 +0300

    all tasks done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Vimeo from '@vimeo/player';
+import Throttle from 'lodash.throttle';
+console.log(Vimeo);
+
+    const iframe = document.querySelector('iframe');
+    const player = new Vimeo(iframe);
+
+    player.on('timeupdate', Throttle((data) => {
+        localStorage.setItem("videoplayer-current-time", data.seconds);}, 1000));
+
+    const pausedTime = localStorage.getItem('videoplayer-current-time');
+    if (pausedTime) {
+         player.setCurrentTime(pausedTime);
+        }
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
 
+import throttle from 'lodash.throttle';
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    input: document.querySelector('input[name="email"]'),
+    message: document.querySelector('textarea[name="message"]'),
+    btnSubmit: document.querySelector('button'),
+    }
+
+const LOCALSTORAGE_KEY = "feedback-form-state";
+
+
+const initialState = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)) || {};
+refs.input.value = initialState.email || '';
+refs.message.value = initialState.message || '';
+
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormInput() {
+  const currentStorage = {
+    email: refs.input.value,
+    message: refs.message.value,
+  };
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(currentStorage));
+}
+
+
+refs.form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault(); 
+  
+  const currentStorage = {
+    email: refs.input.value,
+    message: refs.message.value,
+  };
+  
+  console.log(currentStorage); 
+  
+  localStorage.clear(); 
+  refs.form.reset();
+}
